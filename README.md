754e3898abb0d473a707d2fa00303d4e8355ee05
valdigleis <valdigleis@gmail.com>
2021-12-22 09:05:10 -0300

    basic config

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # prespa
-An implementation of algorithm ReSPA in Python.
+
+*Copyright 2020-2021 Valdigleis S Costa*
+
+<p align="left">
+    <img alt="Github License" src="https://img.shields.io/github/license/valdigleis/prespa" />
+    <img alt="GitHub Issues" src="https://img.shields.io/github/issues/valdigleis/prespa" />
+    <img alt="GitHub Closed Issues" src="https://img.shields.io/github/issues-closed/valdigleis/prespa" />
+    <img alt="GitHub Last Commit" src="https://img.shields.io/github/last-commit/valdigleis/prespa" />
+    <img src="https://img.shields.io/badge/Python-%3E=3.7-blue?logo=python&logoColor=white" alt="Made with Python">
+</p>
+
+
+An implementation of algorithm PReSPA in Python.
--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
+#!/usr/bin/env python
+
+""""Setup logic for pip.""""
+
 from setuptools import setup
+
+def get_long_description():
+    with open('README.md', 'r') as readme_file:
+        return readme_file.read()
 
 setup(
     name='prespa',
     version='0.1-sloth',
     description='An implementation of algorithm ReSPA in Python',
-    url='git@github.com:valdigleis/prespa-sdk-package.git',
-    author='Profº Valdigleis S Costa',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    url='github.com/valdigleis/prespa',
+    author='Valdigleis S Costa',
     author_email='valdigleis@gmail.com',
     license='MIT License',
     packages=['prespa'],
+    entry_points={},
     zip_safe=False
 )
